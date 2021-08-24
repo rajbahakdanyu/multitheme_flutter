74219815ed9ce6a483401a6ba03c29a1455bdc87
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-08-24 16:44:22 +0545

    Finishing up

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,28 @@ import 'package:flutter/material.dart';
 
 import 'package:multitheme/provider/theme_provider.dart';
 import 'package:multitheme/screens/home_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Light & Dark Theme',
-      themeMode: ThemeMode.system,
-      theme: MyThemes.lightTheme,
-      darkTheme: MyThemes.darkTheme,
-      home: HomePage(),
-    );
-  }
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => ThemeProvider(),
+        builder: (context, _) {
+          final themeProvider = Provider.of<ThemeProvider>(context);
+
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Light & Dark Theme',
+            themeMode: themeProvider.themeMode,
+            theme: MyThemes.lightTheme,
+            darkTheme: MyThemes.darkTheme,
+            home: HomePage(),
+          );
+        },
+      );
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:multitheme/provider/theme_provider.dart';
+
+import 'package:multitheme/widget/change_theme_button.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final text = Provider.of<ThemeProvider>(context).themeMode == ThemeMode.dark
+        ? 'DarkTheme'
+        : 'LightTheme';
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Themes'),
+        actions: [
+          ChangeThemeButton(),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          'This is the $text',
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
